74e77500480731172468adb35a23571cff9b90df
nika950 <1nikakankia@gmail.com>
2025-10-12 07:13:25 -0700

    commit

--- level 06/homework.py
+++ level 06/homework.py
@@ -26,7 +26,8 @@ forward(400)
 
 right(90)
 forward(20)
-
+color("darkred")
+begin_fill()
 left(125)
 forward (100)
 
@@ -35,6 +36,11 @@ forward(100)
 
 left(125)
 forward (100)
+
+end_fill()
+
+color("brown")
+
 
 left(180)
 forward(80)
@@ -55,6 +61,10 @@ forward(200)
 right(90)
 forward(35)
 
+color("darkred")
+begin_fill()
+
+
 left(140)
 forward(150)
 
@@ -63,6 +73,11 @@ forward(150)
 
 left(140)
 forward(190)
+
+
+end_fill()
+color("brown")
+
 
 left(180)
 forward(150)
@@ -79,8 +94,13 @@ forward(200)
 right(90)
 forward(35)
 
+color("darkred")
+begin_fill()
+
 left(140)
 forward(150)
+
+
 
 left(80)
 forward(150)
@@ -90,6 +110,11 @@ forward(190)
 
 left(180)
 forward(150)
+
+
+end_fill()
+color("brown")
+
 
 left(90)
 forward(200)
@@ -103,8 +128,16 @@ forward(150)
 right(90)
 forward(20)
 
+color("darkred")
+begin_fill()
+
+
 left(125)
 forward (100)
+
+
+
+
 
 left(110)
 forward(100)
@@ -116,17 +149,27 @@ left(180)
 forward(80)
 
 
+end_fill()
+color("brown")
+
+
+
 left(90)
 forward(400)
 
 left(90)
 forward(380)
+
 end_fill()
 
 #making a dore
 
+
 left(90)
 forward(150)
+
+color("brown")
+begin_fill()
 
 right(90)
 forward(150)
@@ -140,11 +183,15 @@ forward(75)
 right(90)
 forward(150)
 
+
+
 left(180)
 forward(150)
 
 right(90)
-forward(380)
+forward(75)
+end_fill()
+forward(305)
 
 right(90)
 forward(300)
@@ -153,6 +200,7 @@ forward(300)
 
 right(90)
 forward(40)
+
 
 right(90)
 forward(50)
@@ -168,6 +216,7 @@ forward(40)
 
 right(90)
 forward(50)
+
 
 left(90)
 forward(190)
@@ -228,21 +277,29 @@ pendown()
 #making a flag casl
 forward(150)
 
+
+
+
+
 right(90)
 forward(300)
 
 right(90)
 forward(180)
 
+color("darkred")
+begin_fill()
+
 left(135)
 forward(150)
+
 
 left(90)
 forward(150)
 
 left(135)
 forward(180)
-
+end_fill()
 right(90)
 forward(300)
 
@@ -252,19 +309,68 @@ forward(150)
 right(90)
 forward(650)
 
-right(90)
-forward(100)
+color("darkred")
+begin_fill()
 
 right(90)
 forward(100)
+
 right(90)
 forward(100)
+right(90)
+forward(100)
+end_fill()
+ #flag casl finished
 
-#paint the casle 
- 
+
+penup()
+goto(0,50)
+pendown()
+
+goto(0,50)
+goto(0,50)
+
+color("brown")
+
+forward(330)
+color("brown")
+begin_fill()
+right(90)
+forward(150)
+
+left(90)
+forward(80)
+left(90)
+forward(400)
+
+left(90)
+forward(75)
+left(90)
+forward(400)
+end_fill()
+left(180)
+forward(150)
+
+left(90)
+forward(760)
 
 
+color("brown")
+begin_fill()
 
+left(90)
+forward(150)
+left(180)
+forward(400)
+left(90)
+forward(75)
+left(90)
+forward(400)
+left(90)
+forward(75)
+left(90)
+forward(400)
+end_fill()
 
 
 

5dd97f35b1017c96ffc3b9aa1764bdd4e60e42b9
nika950 <1nikakankia@gmail.com>
2026-01-12 10:56:52 -0800

    commit

---
+++ GOA/level 46/claswork.py
@@ -0,0 +1,50 @@
+# events=[ "ww2","yes"]
+# index = int(input())
+
+
+# try:
+#     print(events[index])
+# except IndexError:
+#     print("givrn index is't avelavble in the list")
+# else:
+#     print("event sucsesfuly found")
+# finally:
+#     print("code block of finaly ,will exsecute regardles")
+
+
+# sentence = "1"
+# sentence = [ ]
+
+
+# while sentence !="":
+#     sentence = input("entere a sentence")
+#     sentence.append(sentence)
+# sentence.pop()
+
+# file_name = ""
+
+# with open(file_name, "w", encoding = "utf-8"  ) as file:
+#     for sentence in sentence:
+#         file.write(sentence + "/n")
+
+#         with open (file_name, "r", encoding="utf-8"  )as file:
+#             lines = file.readlines()
+
+#             word_counts = []
+
+#         for line in lines:
+#             line = line.strip()
+        
+#         words = line.strip()
+#         word_counts.append(len(words))
+
+#     longest_line = max(word_counts)
+#     longest_index = word_counts.index(longest_line)
+#     longest =lines[longest_index]
+
+# with open ("clean.txt", "w", encoding  ="utf-8") as file:
+#     for sentance in sencances:
+#         file.write( sentance+ "/n")
+        
+
+

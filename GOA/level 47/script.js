e04889bbc6fb819999ec358a6241d446916c2c1d
nika950 <1nikakankia@gmail.com>
2026-01-14 11:03:51 -0800

    commit

---
+++ GOA/level 47/script.js
@@ -0,0 +1,21 @@
+let mod = document.body.childrens[1]
+let myP = Document.body.children[0]
+
+myP.style.fontsize = '40px'
+
+mybutton.style.backgroundcolor = "red"
+mybutton.style.color = "red"
+mybutton.style.pading = "50px"
+mybutton.style.border = "5px"
+
+let person = {
+    firstname:"nika",
+    lastname:"kankia",
+    hobby: "coding",
+    favoritemuvie: "fast and furious",
+}
+
+consol.log(person.firstname)
+consol.log(person.lastname)
+consol.log(person.hobby)
+consol.log(person.favoritemuvie)

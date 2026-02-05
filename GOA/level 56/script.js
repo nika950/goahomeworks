468321fd225ce89661f420e87d1c86b08c3b559d
nika950 <1nikakankia@gmail.com>
2026-02-05 05:12:14 -0800

    commit

---
+++ GOA/level 56/script.js
@@ -0,0 +1,92 @@
+let playerOptionButtns = document.getElementById("olayeer")
+count =compiuterOptions = ["rock", "paper", "sizer"]
+
+console.log(m=Math.rendom())
+
+function play(e) {
+    comsol.log(e)
+
+
+    const randomIndex = Math.flor (Math.random()* 3)
+    const computerChoice = compiuterOptions[randomIndex ]
+}
+
+
+
+playerOptionButtns.addEventListiner("click", function(e){
+
+})
+
+
+// HOMEWORK
+
+
+for (let i = 4; i<= 4;){
+    console.log(i);
+}
+
+let asnswer = ""
+let desierd = "yes"
+
+while (answer !== desierd) {
+    answer = prompt("enter your answer")
+}
+
+        8
+for (let i = 0; i <=20; i++){
+    console.log(i);
+}
+
+
+
+           //9
+for(let i = 0;i <=10; i+=2){
+    console.log(i)
+}
+
+        10
+for(let i = 0; i <=100; i++){
+    console.log(i)
+}
+
+        17
+let counter = 10 
+while (counter >=0){
+    console.log(counter);
+    copunter --; 
+}
+console.log("count down complited!");
+
+        18
+let number = 25;
+if(number % 3 === 0 && number % 5 === 0 ){
+    console.log("FizzBuzz");
+}
+else if (number % 3 === 0){
+    console.log("Fizz");
+}
+else if (number % 5 == 0){
+    console.log("Buzz");
+}
+else{
+    console.log(number);
+}
+
+
+
+let age = Number (prompt("enter your age"))
+
+let name = prompt("ra gqvia siyvarulo");
+console.log("salami, " + name );
+
+
+
+if (choice === "sunny") {
+    if (temperature < 86) {
+    para.textContent = `It is ${temperature} degrees outside — nice and sunny. Let's go out to the beach, or the park, and get an ice cream.`;
+    } else if (temperature >= 86) {
+    para.textContent = `It is ${temperature} degrees outside — REALLY HOT! If you want to go outside, make sure to put some sunscreen on.`;
+    }
+}
+
+

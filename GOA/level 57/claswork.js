cb93adc1a2b3a43403fbb7549246cd58a8e28aa4
nika950 <1nikakankia@gmail.com>
2026-02-07 11:03:49 -0800

    commit

---
+++ GOA/level 57/claswork.js
@@ -0,0 +1,66 @@
+// let book = {
+//     title: "ivaneho",
+//     autor: "gela",
+    
+//     getAutor:function(){
+//         console.log("autor of the book")
+//     }
+// }
+
+// console.log(book.title)
+// console.log(book.autor)
+// console.log(book.langth)
+// console.log(book.autor)
+
+
+// function book(title, autor, year, pages, favoritepage){
+//     this.title = title;
+//     this.autor = autor;
+//     this.years = years;
+//     this.pages =pages;
+// }
+
+// apple_and_banan= book("pinaple","gela", -67,6767,67)
+// deda = book("family","mama",1986,"10+",67)
+
+// console.log(apple_and_banana,deda)
+
+
+// vinaxavt informacias da aris primitiuli da kompleqsuri tipebi 
+//javscriptshi aris ori dziritadi mexsiereba stack da heap
+
+
+// firstName = {
+//     title:"facebook"
+// }
+
+
+// let firstName = secondName
+// secondName.title="goa network on top"
+
+
+// console.log(firstName)
+// console.log (secondName) 
+
+
+
+// let languages = ["html", "css","js",]
+
+
+// languages.push("phiton")
+// languages.pop(1)
+// console.log(languages)
+
+
+let DaraBase = [];
+
+const sedBtn = sendBtn = document.getElementsByClassName("registrationForm");
+form.addeventlistiner("submit",function(event){
+    event.preventDefault();
+    const nameValue = document.getElementsByClassName("name").value;
+    const emailValue = document.getElementsByClassName("email").value;
+    const paswordValue = document.getElementsByClassName("pasword").value;
+
+
+    const newUser = new Account(nameValue, emailValue, paswordValue)
+});

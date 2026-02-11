0ad3d1cfb4bcfe5d431949fc6b3c18434a0b825d
nika950 <1nikakankia@gmail.com>
2026-02-11 11:05:45 -0800

    commit

---
+++ GOA/level 59/clas.js
@@ -0,0 +1,8 @@
+// const arr = [1,2,3,4,5,6,7,8,9,]
+
+// console.log(arr.reverse)
+// console.log(arr.toSorted)
+// console.log(arr.join("22"))
+// console.log(arr.pop(5,7))
+// console.log(arr.indexOf(5))
+console.log("helow")
---
+++ GOA/level 60/class.js
@@ -0,0 +1,31 @@
+// function fibbonaci(n){ 
+//     let arr = [];
+//     for( let i = 0; i<n; i++){
+//         console.log(arr[-1])
+//         arr[i-1]+ arr[i-2];
+//         arr.push(newElement)
+//     };
+//     return arr;
+// }
+
+// console.log(fibbonaci(7))
+
+
+
+
+let username = "nika"
+
+
+username = username.split("")
+
+for(let  i = 0 username.length - 1; i >= 0; i--) {
+    username.push(Number(username[i]))
+}
+console.log(result)
+
+
+
+
+// console.log(username.)
+// console.log(username.)
+// console.log(username.)
